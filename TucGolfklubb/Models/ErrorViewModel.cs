ed6e7391620b2437f0581e203bfaca53f4d73868
Emelie <emelieaxi@hotmail.com>
2025-03-20 09:02:11 +0100

    Add project files.

---
+++ TucGolfklubb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TucGolfklubb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

65b758b7f5a06a2227ba5b13621237cf308e5366
Emilia <emiliaalmgren@hotmail.com>
2025-03-23 20:54:56 +0100

    Skapa DatabasContext, ContextLogger och ContextExtension och konfigurering av EF Core

---
+++ TucGolfklubbDataContext/TucGolfContextLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Environment;
+
+namespace TucGolfklubb.DataContext.SqlServer
+{
+        public class TucGolfContextLogger
+        {
+            public static void WriteLine(string message)
+            {
+                string path = Path.Combine(GetFolderPath(SpecialFolder.DesktopDirectory), "tucgolflog.txt");
+
+                StreamWriter textFile = File.AppendText(path);
+                textFile.WriteLine(message);
+                textFile.Close();
+            }
+
+        }
+}

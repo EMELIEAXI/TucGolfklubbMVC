4aa1e7909000bf25066ea20418dcab2060011ba8
Emelie <emelieaxi@hotmail.com>
2025-04-01 14:17:44 +0100

    Har lagt till och länkat till Kontaktsidan

---
+++ TucGolfklubb/Controllers/KontaktController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TucGolfklubb.Controllers
+{
+    public class KontaktController : Controller
+    {
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+    }
+}

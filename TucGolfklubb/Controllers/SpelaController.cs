9d90697341d780b1c1097e371050f5f0aa255115
Emelie <emelieaxi@hotmail.com>
2025-04-01 15:47:17 +0100

    Lagt till och länkat Spela Golf

---
+++ TucGolfklubb/Controllers/SpelaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TucGolfklubb.Controllers
+{
+    public class SpelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Greenfee()
+        {
+            return View();
+        }
+
+        public IActionResult Foretag() 
+        {
+            return View();
+        }
+    }
+}

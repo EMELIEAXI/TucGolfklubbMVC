663b5fb605ed7a0a91a0a0c5edc44e15e067cdf5
MikaelGille <mikael.gille@gmail.com>
2025-04-01 11:56:20 +0200

    Uppdaterad product-modell

--- TucGolfklubb/Models/Product.cs
+++ TucGolfklubb/Models/Product.cs
@@ -3,14 +3,16 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Description { get; set; } = "";
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public ICollection<Review>? Reviews { get; set; }
-        public ICollection<OrderItem>? OrderItems { get; set; }
+        public string Image { get; set; } = "default.jpg";
+        public int Stock { get; set; } = 0;
 
         public int? CategoryId { get; set; }
         public Category? Category { get; set; }
 
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
 }

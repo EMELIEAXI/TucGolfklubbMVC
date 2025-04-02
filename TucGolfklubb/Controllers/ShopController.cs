69c9db576f19dac17991f1574e300db60c94053b
Emilia <emiliaalmgren@hotmail.com>
2025-04-02 21:26:24 +0200

    Dynamisk produktvy

--- TucGolfklubb/Controllers/ShopController.cs
+++ TucGolfklubb/Controllers/ShopController.cs
@@ -1,17 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TucGolfklubb.Data;
+using TucGolfklubb.Models;
 
 namespace TucGolfklubb.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult ProductShop()
+        public async Task<IActionResult> ProductShop(int? categoryId)
         {
-            return View();
+            var categories = await _context.Categories.ToListAsync();
+
+            //Hämta produkter baserat på kategori-Id
+            var products = categoryId.HasValue
+                ? await _context.Products.Where(p => p.CategoryId == categoryId.Value).ToListAsync()
+                : await _context.Products.ToListAsync();
+
+            var viewModel = new ProductShopViewModel
+            {
+                Categories = categories,
+                Products = products,
+                SelectedCategoryId = categoryId
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ TucGolfklubb/Models/ProductShopViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TucGolfklubb.Models;
+using System.Collections.Generic;
+
+namespace TucGolfklubb.Models
+{
+    public class ProductShopViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public int? SelectedCategoryId { get; set; }
+    }
+}

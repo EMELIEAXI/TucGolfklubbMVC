7dad79f2ddfe9c05b588f79c897fada57d6eddc4
Emelie <emelieaxi@hotmail.com>
2025-04-08 16:27:39 +0100

    Lagt till Nyhetssidan

---
+++ TucGolfklubb/Controllers/NyheterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TucGolfklubb.Controllers
+{
+    public class NyheterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

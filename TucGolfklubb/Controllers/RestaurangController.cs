d0bebb58a49df1461bc138d21f3cc6b68eab9ece
Emelie <emelieaxi@hotmail.com>
2025-04-09 08:47:05 +0100

    Lagt till Restaurang

---
+++ TucGolfklubb/Controllers/RestaurangController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TucGolfklubb.Controllers
+{
+    public class RestaurangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

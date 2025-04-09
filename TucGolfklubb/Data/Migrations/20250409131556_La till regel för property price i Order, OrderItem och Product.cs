4bb201ca7e15d3c16f11957e2b64c5fef523213d
MikaelGille <mikael.gille@gmail.com>
2025-04-09 15:54:46 +0200

    Uppdaterad index och css för ProductShop

---
+++ TucGolfklubb/Data/Migrations/20250409131556_La till regel för property price i Order, OrderItem och Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TucGolfklubb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LatillregelförpropertypriceiOrderOrderItemochProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

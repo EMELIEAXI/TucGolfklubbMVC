5331bdee96e21fa57a244b8391f6658d5d455fdc
Emelie <emelieaxi@hotmail.com>
2025-04-09 21:32:42 +0100

    La till Migration för Price i OrderItem hade försvunnit?

---
+++ TucGolfklubb/Data/Migrations/20250409202715_AddPriceToOrderItem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TucGolfklubb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToOrderItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+            name: "Price",
+            table: "OrderItems",
+            type: "decimal(18,2)",
+            nullable: false,
+            defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

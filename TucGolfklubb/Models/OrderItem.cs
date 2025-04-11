37984c9e1f53c092fcdd85c096a6a65ee343c8e4
Gene Suelo <gene.suelo@gmail.com>
2025-04-12 01:55:12 +0200

    Uppdatera Hantera Ordrar i Adminpanel för att kunna ta bort, redigera och se kvittot av beställningarna om det skulle ske några ändringar.

--- TucGolfklubb/Models/OrderItem.cs
+++ TucGolfklubb/Models/OrderItem.cs
@@ -12,8 +12,7 @@
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public string? ProductName { get; set; }
+        public decimal TotalPrice => Quantity * Price;
 
-        //Gene: Add this to store the price of the product at purchase time
-        //public decimal UnitPrice { get; set; }
     }
 }
